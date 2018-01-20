926b4a429360099238735e52f10f36b7242fcc97
Brandon Matthews <brandonmatthews1982@gmail.com>
2018-01-20 10:49:45 -0500

    Sticky Saved Players section

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -210,3 +210,14 @@ $(document).ready(function() {
 
 
 });
+$(document).ready(function () {  
+    var top = $('.sticky-scroll-box').offset().top;
+    $(window).scroll(function (event) {
+      var y = $(this).scrollTop();
+      if (y >= top)
+        $('.sticky-scroll-box').addClass('fixed');
+      else
+        $('.sticky-scroll-box').removeClass('fixed');
+      $('.sticky-scroll-box').width($('.sticky-scroll-box').parent().width());
+    });
+  });
